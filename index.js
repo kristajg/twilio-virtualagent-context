e97a81c681a83b3230b8323283c8abd59f6aad23
Krista Goralczyk <kristagoralczyk@c02fg4kumd6m.attlocal.net>
2021-08-16 19:22:36 -0500

    Add expres server with three endpoints to store and pass dialogflow context

---
+++ index.js
@@ -0,0 +1,61 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/dialogflow-fulfillment-webhook', (req, res) => {
+  console.log('Dialogflow callback hit');
+  return res.json({
+    fulfillmentText: `Perfect, thank you`, // Dialogflow's closing remarks before switching back to Studio
+    end_interaction: true, // Manually ends the caller interaction with Dialogflow
+  });
+});
+
+// Storing in a global variable is the easiest way to demonstrate passing context
+// However, it is ephemeral and not secure. 
+// Other options for data storage: database, redis, JSON / text file??
+let generalData = '';
+let stepOneData = '';
+let stepTwoData = '';
+app.post('/virtual-agent-callback', (req, res) => {
+  console.log('Virtual Agent callback hit');
+  const {
+    query: {
+      step = '',
+    },
+    body: {
+      QueryText = '',
+    },
+  } = req;
+  console.log('QueryText from Dialogflow is: ', QueryText);
+  if (QueryText) {
+    if (step === 'one') {
+      stepOneData = QueryText;
+    }
+    if (step === 'two') {
+      stepTwoData = QueryText;
+    }
+    generalData = QueryText;
+  }
+});
+
+app.post('/get-stored-dialogflow-data', (req, res) => {
+  console.log('Get stored dialogflow data hit');
+  const { step } = req.query;
+  let payload = {};
+  if (step === 'one') {
+    payload = { stepOneData };
+  } else if (step === 'two') {
+    payload = { stepTwoData };
+  } else {
+    payload = { generalData };
+  }
+  res.json(payload);
+});
+
+http.createServer(app).listen(8081, () => {
+  console.log('Express server listening on port 8081');
+});
